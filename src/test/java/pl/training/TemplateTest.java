c62efa418ff968f5240d707eb67db3b9ab57f3ae
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-04-25 23:13:01 +0200

    Template

--- src/test/java/pl/training/TemplateTest.java
+++ src/test/java/pl/training/TemplateTest.java
@@ -11,6 +11,8 @@ import static org.junit.Assert.*;
 
 public class TemplateTest {
 
+    private static final String EXPECTED_RESULT = "My name is John Smith";
+
     private Template template = new Template("My name is ${firstName} ${lastName}");
     private Map<String, String> parameters = new HashMap<>();
 
@@ -22,12 +24,18 @@ public class TemplateTest {
 
     @Test
     public void shouldEvaluateTextWithExpressions() {
-        assertEquals(template.evaluate(parameters), "My name is John Smith");
+        assertEquals(EXPECTED_RESULT, template.evaluate(parameters));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionWhenParameterIsMissing() {
         template.evaluate(new HashMap<>());
+    }
+
+    @Test
+    public void shouldIgnoreAdditionalParameters() {
+        parameters.put("extraParameter", "someValue");
+        assertEquals(EXPECTED_RESULT, template.evaluate(parameters));
     }
 
 }
